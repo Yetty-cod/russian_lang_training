541d6066ccfb17b152e31079d1f8a86166344366
Yetty-code <klisak.e@yandex.ru>
2026-03-27 18:00:00 +0300

    Создана общая структура программы

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('писюн')
+import sqlite3
+
+
+def main():
+    connection = sqlite3.connect('tasks.sqlite')
+    cursor = connection.cursor()
+
+    # code of program will be here
+
+    connection.close()
+
+
+if __name__ == 'main':
+    main()
